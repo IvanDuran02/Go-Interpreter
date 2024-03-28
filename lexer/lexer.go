c33ee1ec1b56349e2d486d083a9677b3b088f26a
Ivan Duran <iamivanduran02@gmail.com>
2024-03-27 23:10:01 -0400

    added keyword and identifcation, and skips whitespace and newline etc..

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,71 @@
 package lexer
 
+import (
+	"interpreter/token"
+)
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+		// 0 == nil since byte
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	// note that in Go '' represents a string literal and will use Unicode points
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
 type Lexer struct {
 	input        string
 	position     int  // current pos in input (points to current char)
@@ -9,15 +75,22 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
+// (l *Lexer) indicated this is a method of the Lexer struct
+// The star indicated it uses the pointer and doesn't make a copy
+// Meaning the method can modify the Lexer object
 func (l *Lexer) readChar() {
+	// Check end of input to see if it is end of file or not
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
+		// Next character
 		l.ch = l.input[l.readPosition]
 	}
+	// Updates postion to the next character
 	l.position = l.readPosition
 	l.readPosition += 1
 }
